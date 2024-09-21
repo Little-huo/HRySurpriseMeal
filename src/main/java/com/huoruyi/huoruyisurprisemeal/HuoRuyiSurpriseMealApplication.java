597b869dfa6b3da65802f4c1bbb70d6248d2a24f
LittleHuo <3466691370@qq.com>
2024-09-22 00:38:39 +0800

    第一次提交

---
+++ src/main/java/com/huoruyi/huoruyisurprisemeal/HuoRuyiSurpriseMealApplication.java
@@ -0,0 +1,13 @@
+package com.huoruyi.huoruyisurprisemeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuoRuyiSurpriseMealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuoRuyiSurpriseMealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huoruyi/huoruyisurprisemeal/HuoRuyiSurpriseMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huoruyi.huoruyisurprisemeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuoRuyiSurpriseMealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
