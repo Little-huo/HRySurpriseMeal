e7a273353eb752b4c54abcc06a661bbf1e4d1034
LittleHuo <3466691370@qq.com>
2024-09-22 15:27:15 +0800

    @SpringBootApplication注解的使用

--- src/main/java/com/huoruyi/HuoRuyiSurpriseMealApplication.java
+++ src/main/java/com/huoruyi/HuoRuyiSurpriseMealApplication.java
@@ -8,6 +8,7 @@ public class HuoRuyiSurpriseMealApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HuoRuyiSurpriseMealApplication.class, args);
+        System.out.println("hello world!");
     }
 
 }
